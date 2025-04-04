325f60f03ad7fe70670bcc984ba445338804f031
Ananthu Ajayan <137896002+ananthuajayan@users.noreply.github.com>
2025-04-04 16:51:45 +0530

    lastchange

--- src/api/services/authService.js
+++ src/api/services/authService.js
@@ -20,6 +20,19 @@ export const register = async (formData) => {
     throw error;
   }
 };
+export const registerWithOpt = async (formData) => {
+  try {
+    const response = await axiosInstance.post(API_ENDPOINTS.REGISTER_WITH_OTP, formData);
+    console.log("response---->>>???????",response);
+    return response.data;
+  } catch (error) {
+    // Log for devs
+    console.error("Register API failed:", error);
+
+    // Re-throw if you want to handle it in the component
+    throw error;
+  }
+};
 
 
 export const logout = () => {
