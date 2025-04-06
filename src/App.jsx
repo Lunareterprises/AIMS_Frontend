11024aded87f3eb865acc98b17a054ef63601dcd
Arunthathy <arunthathy@glidix.com>
2025-04-06 16:07:28 +0530

    side menu and nav bar and breadcrumb set

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,9 @@
-
-import './App.css'
-import { Routes, Route } from 'react-router-dom'
-import LandingPage from './pages/landingPage/LandingPage'
-import Signup from './pages/authentication/Signup'
-import Login from './pages/authentication/Login'
-import OrganizationProfile from './pages/organizationProfile/OrganizationProfile'
-
+// src/App.jsx
+import './App.css';
+import AppRoutes from './routes/AppRoutes';
 
 function App() {
-  return (
-  <Routes>
-      <Route path="LandingPage" element={<LandingPage />} />
-      <Route path="Signup" element={<Signup />} />
-      <Route path="/" element={<Login />} />
-      <Route path="/OrganizationProfile" element={<OrganizationProfile />} />
-      
-
-
-  </Routes>
-  )
+  return <AppRoutes />;
 }
 
-export default App
+export default App;
